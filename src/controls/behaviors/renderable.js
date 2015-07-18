749fd7ae59fe3a974b4a4d16fc9dc22c25703574
Vitaliy Makeev <w.makeev@gmail.com>
2015-07-19 00:32:26 +0500

    update

--- src/controls/behaviors/renderable.js
+++ src/controls/behaviors/renderable.js
@@ -1,10 +1,11 @@
 import stampit from 'stampit';
-import { build } from 'domjs';
+import domjs from 'domjs';
 
+let dom = domjs(document);
 
 export default stampit().methods({
   render() {
-    this.el = build(this.template.bind(this));
+    this.el = dom.build(this.template.bind(this));
     return this.el;
   }
 });
